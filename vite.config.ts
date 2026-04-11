a3fae587bc70527c216249d92b6b3c215c0d02a1
tiodio324 <nikita324smirnovvv@gmail.com>
2026-04-11 17:55:56 +0300

    ВКР

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TaskManagement-15/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
